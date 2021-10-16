9f819360ea85978abeaa9ab499ab84238d2fe628
Gastt <olugbengagt@gmail.com>
2021-10-16 09:56:39 +0100

    Starting test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
 import logo from './logo.svg';
+import gastt from './asset/gastt.jpg'
+import icon from './asset/icon.svg'
+import hot from './asset/hot.svg'
+import community from './asset/community.svg'
+import bag from './asset/bag.svg'
 import './App.css';
 
 function App() {
@@ -6,6 +11,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={gastt} className="App-logo" alt="logo" />
+        <img src={bag} className="App-logo" alt="logo" />
+        <img src={icon} className="App-logo" alt="logo" />
+        <img src={community} className="App-logo" alt="logo" />
+        <img src={hot} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
